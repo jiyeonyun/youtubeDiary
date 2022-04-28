1138aa6663535d1291b15684161e726b2d7021ce
jiyeon <jiyeonyun43@gmail.com>
2022-04-28 11:02:39 +0900

    video 재랜더링 막음

--- src/components/writeModal/writeModal.jsx
+++ src/components/writeModal/writeModal.jsx
@@ -4,10 +4,14 @@ const WriteModal = ({WriteChange}) =>{
     const[inputVal,setInputVal] = useState('');
     console.log(inputVal);
     let write = inputVal;
+    const click = (e)=>{
+        e.stopPropagation();
+        WriteChange(write)
+    }
     return(
         <div className={styles.modal}>
-            <input onChange={(e)=>setInputVal(e.target.value)} className={styles.textArea} name="" id="" cols="30" rows="10"></input>
-            <button onClick={WriteChange(write)}>go</button>
+            <input onChange={(e)=>setInputVal(e.target.value)} className={styles.textArea} cols="30" rows="10"></input>
+            <button onClick={click}>go</button>
         </div>
     )
 };
